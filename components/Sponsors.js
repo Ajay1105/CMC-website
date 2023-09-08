e7989c8c0d6b75821be7a7f703ccf71938342adc
Ajay <aky11052003@gmail.com>
2023-09-08 22:10:40 +0530

    resolved conflict

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -3,71 +3,21 @@ import Image from "next/image";
 
 const Sponsors = () => {
   return (
-<<<<<<< HEAD
     <div className="flex flex-col justify-around items-center align-middle my-4">
-     <h1 className=" font-semibold font-sans text-4xl p-8 text-white">Sponsors</h1>
-    <h1 className=" text-5xl font-semibold text-white mb-20">To be announced...</h1>
-=======
-    <>
-      <div className="flex flex-col justify-around items-center align-middle my-4">
-    <h1 className=" font-semibold font-sans text-4xl p-8 text-white">Sponsors</h1>
-    <h1 className=" font-semibold font-sans text-2xl py-4 text-white">Gold</h1>
-      <div className="flex flex-wrap items-center align-middle justify-around">
-      
-        <Image
-          className="mx-20 my-20"
-          src={Rtist}
-          height={250}
-          width={250}
-          alt="Rtist"
-        />
-        <Image
-          className="mx-20 my-20"
-          src={CMCPAC}
-          height={250}
-          width={250}
-          alt="CMCPAC"
-        />
-      </div>
-      <h1 className=" font-semibold font-sans text-2xl py-12 text-white">Silver</h1>
-      <div className="flex flex-wrap items-center align-middle justify-around">
-        <Image
-          className="mx-10 my-10"
-          src={Rtist}
-          height={200}
-          width={200}
-          alt="Rtist"
-        />
-        <Image
-          className="mx-10 my-10"
-          src={CMCPAC}
-          height={200}
-          width={200}
-          alt="CMCPAC"
-        />
-        <Image
-          className="mx-10 my-10"
-          src={Rtist}
-          height={200}
-          width={200}
-          alt="Rtist"
-        />
-        <Image
-          className="mx-10 my-10"
-          src={CMCPAC}
-          height={200}
-          width={200}
-          alt="CMCPAC"
-        />
-      </div>
->>>>>>> 05e7b949fd87a377814202e6b35eb1ff6f72308a
+      <h1 className=" font-semibold font-sans text-4xl p-8 text-white">
+        Sponsors
+      </h1>
+      <h1 className=" text-5xl font-semibold text-white mb-20">
+        To be announced...
+      </h1>
     </div>
+
     // <>
     //   <div className="flex flex-col justify-around items-center align-middle my-4">
     // <h1 className=" font-semibold font-sans text-4xl p-8 text-white">Sponsors</h1>
     // <h1 className=" font-semibold font-sans text-2xl p-8 text-white">Gold</h1>
     //   <div className="flex flex-wrap items-center align-middle justify-around">
-      
+
     //     <Image
     //       className="mx-20 my-20"
     //       src={Rtist}
@@ -116,7 +66,7 @@ const Sponsors = () => {
     //   </div>
     // </div>
     // </>
-  )
-}
+  );
+};
 
-export default Sponsors
+export default Sponsors;
