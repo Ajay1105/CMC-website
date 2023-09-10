0a69b9bfd4e2f42b138d9eb4ee77f80bf335203b
JasnoorMaan <jasnoorm0001@gmail.com>
2023-09-10 17:34:06 +0530

    timeline bug fixed

--- components/Timeline.js
+++ components/Timeline.js
@@ -6,11 +6,11 @@ const Timeline=()=>{
             
             <div className='flex flex-col md:w-full py-8 justify-center items-center w-4/5'>
                 <div className='flex flex-col text-white shadow-3xl font-medium sm:w-4/5 rounded-[12px]'>
-                    <div className='flex justify-between my-4 px-8 py-6 blurBlackground'><div>09 Sep 2023 | 5:00 PM</div><div>Hackathon registration starts</div></div>
-                    <div className='flex justify-between my-4 px-8 py-6 blurBlackground'><div>30 Sep 2023 | 5:00 PM</div><div>Hackathon registration closes</div></div>
-                    <div className='flex justify-between my-4 px-8 py-6 blurBlackground'><div>13 Oct 2023 | 5:00 PM</div><div>Introductory Event & Pitching of Ideas</div></div>
-                    <div className='flex justify-between my-4 px-8 py-6 blurBlackground'><div>14 Oct 2023 | 5:00 PM</div><div>Pitch Practice sessions</div></div>
-                    <div className='flex justify-between my-4 px-8 py-6 blurBlackground'><div>15 Oct 2023 | 5:00 PM</div><div>Final Presentation & Result Declaration</div></div>
+                    <div className='flex flex-wrap justify-between my-4 px-8 py-6 blurBlackground '><div className="pb-4">09 Sep 2023 | 5:00 PM</div><div>Hackathon registration starts</div></div>
+                    <div className='flex flex-wrap justify-between my-4 px-8 py-6 blurBlackground '><div className="pb-4">30 Sep 2023 | 5:00 PM</div><div>Hackathon registration closes</div></div>
+                    <div className='flex flex-wrap justify-between my-4 px-8 py-6 blurBlackground '><div className="pb-4">13 Oct 2023 | 5:00 PM</div><div>Introductory Event & Pitching of Ideas</div></div>
+                    <div className='flex flex-wrap justify-between my-4 px-8 py-6 blurBlackground '><div className="pb-4">14 Oct 2023 | 5:00 PM</div><div>Pitch Practice sessions</div></div>
+                    <div className='flex flex-wrap justify-between my-4 px-8 py-6 blurBlackground '><div className="pb-4">15 Oct 2023 | 5:00 PM</div><div>Final Presentation & Result Declaration</div></div>
                 </div>
             </div>
             
