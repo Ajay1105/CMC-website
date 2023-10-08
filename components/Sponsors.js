f34b29e7d56cc63125f286ecb6b85a698d7b8691
Ajay <aky11052003@gmail.com>
2023-10-08 21:16:03 +0530

    added sponsors website link

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -23,55 +23,67 @@ const Sponsors = () => {
       </h1>
       <div className="flex flex-row flex-wrap justify-around items-center align-middle my-4">
         <div className="box ml-16 mr-16 mt-16 mb-16 glowing rounded-lg">
-          <Image
-            className="rounded-lg"
-            src={amura}
-            height={250}
-            width={250}
-            alt="AMURA LOGO"
-          />
+          <a href="https://amura.ai/">
+            <Image
+              className="rounded-lg"
+              src={amura}
+              height={250}
+              width={250}
+              alt="AMURA LOGO"
+            />
+          </a>
         </div>
         <div className=" ml-16 mr-16 box glowing rounded-lg">
-          <Image
-            className="rounded-lg"
-            src={mice2}
-            height={250}
-            width={250}
-            alt="MICE LOGO"
-          />
+          <a href="https://www.micelabs.in/">
+            <Image
+              className="rounded-lg"
+              src={mice2}
+              height={250}
+              width={250}
+              alt="MICE LOGO"
+            />
+          </a>
         </div>
         <div className=" ml-16 mt-10 mr-16 box glowing rounded-lg">
-          <Image
-            className="rounded-lg"
-            src={orangewood}
-            height={250}
-            width={250}
-            alt="MICE LOGO"
-          />
+          <a href="https://www.orangewood.co/">
+            <Image
+              className="rounded-lg"
+              src={orangewood}
+              height={250}
+              width={250}
+              alt="MICE LOGO"
+            />
+          </a>
         </div>
       </div>
       <div className="flex flex-wrap justify-around items-center align-middle">
-        <Image
-          className="mx-10 my-10"
-          src={Hll}
-          height={250}
-          width={250}
-          alt="DEVFOLIO LOGO"
-        />
-        <Image
-          className="mx-10 my-10"
-          src={navan}
-          height={250}
-          width={250}
-          alt="DEVFOLIO LOGO"
-        />
-        <Image
-          className="mx-10 my-10"
-          src={Amsa}
-          height={250}
-          width={250}
-          alt="DEVFOLIO LOGO"
-        />
+        <a href="https://www.lifecarehll.com/">
+          <Image
+            className="mx-10 my-10"
+            src={Hll}
+            height={250}
+            width={250}
+            alt="DEVFOLIO LOGO"
+          />
+        </a>
+        <a href="https://navan.ai/">
+          <Image
+            className="mx-10 my-10"
+            src={navan}
+            height={250}
+            width={250}
+            alt="DEVFOLIO LOGO"
+          />
+        </a>
+        <a href="https://www.amsaindia.in/">
+          <Image
+            className="mx-10 my-10"
+            src={Amsa}
+            height={250}
+            width={250}
+            alt="DEVFOLIO LOGO"
+          />
+        </a>
         <Image
           className="mx-10 my-10"
           src={devfolio}
